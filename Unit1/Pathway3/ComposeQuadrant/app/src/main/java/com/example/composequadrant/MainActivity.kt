ce2f91ab65ca14c1b353ce4103f8abf9ee776426
seo <eskai84@gmail.com>
2022-11-18 18:09:20 +0900

    [Unit 1] 사분면1

--- Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,19 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,9 +23,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeQuadrantApp() {
+    val title1 = stringResource(R.string.first_title)
+    val title2 = stringResource(R.string.second_title)
+    val title3 = stringResource(R.string.third_title)
+    val title4 = stringResource(R.string.fourth_title)
+    val desc1 = stringResource(R.string.first_description)
+    val desc2 = stringResource(R.string.second_description)
+    val desc3 = stringResource(R.string.third_description)
+    val desc4 = stringResource(R.string.fourth_description)
+
     Column() {
-        Row() { }
-        Row() { }
+        Row() {
+            ComposableInfoCard(title1,desc1,Color.Green,Modifier)
+            ComposableInfoCard(title1,desc1,Color.Yellow,Modifier)
+        }
+        Row() {
+            ComposableInfoCard(title1,desc1,Color.Cyan,Modifier)
+            ComposableInfoCard(title1,desc1,Color.LightGray,Modifier)
+        }
     }
 }
 
@@ -41,10 +51,23 @@ private fun ComposableInfoCard(
     backgroundColor: Color,
     modifier: Modifier = Modifier
 ) {
-    Column( ) { }
+    Column( modifier = Modifier.background(backgroundColor) ) {
+        Text(
+            text = title,
+            modifier = modifier
+        )
+        Text(
+            text = description,
+            modifier = modifier
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeQuadrantTheme{
+        ComposeQuadrantApp()
+    }
+}
